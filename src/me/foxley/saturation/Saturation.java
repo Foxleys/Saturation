a910f0f0bed4630514d907fc20bf628abb2e1e9d
Foxley <le.roux.adrien@gmail.com>
2020-07-22 22:09:09 +0200

    Initial commit

---
+++ src/me/foxley/saturation/Saturation.java
@@ -0,0 +1,19 @@
+package me.foxley.saturation;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Saturation extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        this.getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onFoodLevelChange(FoodLevelChangeEvent event) {
+        event.setFoodLevel(20);
+    }
+}
